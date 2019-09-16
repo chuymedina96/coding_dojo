db2db7812f09a1cb72e10e06004238dc8321d2e5
neo <chuymedina96@gmail.com>
2019-09-16 13:00:00 -0500

    functions_int2

---
+++ chicago_codes_bootcamp/chicago_codes_python/python-practice/parsing_lesson.py
@@ -0,0 +1,37 @@
+# customers = [[20,50], [35,10],[0,8]]
+
+
+
+# print(customers)
+# print(customers[1])
+# print(customers[1][1])
+# customers[1][1] =15
+# print(customers)
+
+
+
+
+
+#Dictionaries
+    #Lists of Dictionaries
+
+vips =[
+    {'name': 'Bob', 'location': [20,20]},
+    {'name': 'Jane', 'location':[10,2]}
+]
+#print(vips) # list of dictionaries
+print(vips[0]['name']) # of dictionary
+print(vips[1]['name'])
+vips[1]['name'] = "Robert"
+print(vips[1])
+print(vips)
+
+my_faves = {
+    'food': 'sushi',
+    'color': 'gray'
+}
+
+print(my_faves['food'])
+my_faves['food'] = 'sandwiches'
+print(my_faves[])
+
