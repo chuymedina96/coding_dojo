aeb6980dc17ddac97d1abea2c8323c2d99562f18
neo <chuymedina96@gmail.com>
2019-09-17 14:04:10 -0500

    fixed display_user_balance function, no need to pass in extra parameter, for we just wanted to return the account balance. Finished chaining functions from user.py

--- chicago_codes_bootcamp/chicago_codes_python/python_stack/python/oop/chaining_methods.py
+++ chicago_codes_bootcamp/chicago_codes_python/python_stack/python/oop/chaining_methods.py
@@ -1,3 +1,5 @@
+#UPDATED ASSIGNMENT USING CHAINING
+
 class User:
     def __init__(self, name, email):
         self.name = name
@@ -6,10 +8,13 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
     def make_withdrawl(self, amount):
         self.account_balance -= amount
+        return self
     def display_user_balance(self):
         print(self.account_balance)
+        return self
     # def transfer_money(self, other_user, amount):
     #     self.account_balance -= self.account_balance - amount
     #     self.other_user = self.other_user + amount
@@ -17,31 +22,21 @@ class User:
 
 michael = User("michael", "michael96@gmail.com")
 
-michael.make_deposit(100)
-michael.make_deposit(100)
-michael.make_deposit(100)
-michael.make_withdrawl(50)
-michael.display_user_balance()
+michael.make_deposit(100).make_deposit(100).make_deposit(100).make_withdrawl(50).display_user_balance()
 
 
 chuy = User("chuy", "chuymedina96@gmail.com")
 
-
-chuy.make_deposit(20)
-chuy.make_deposit(20)
-chuy.make_withdrawl(10)
-chuy.display_user_balance()
+chuy.make_deposit(20).make_deposit(20).make_withdrawl(10).display_user_balance()
 
 
 jovanny = User("jovanny", "test96@gmail.com")
 
-jovanny.make_deposit(100)
-jovanny.make_withdrawl(10)
-jovanny.make_withdrawl(10)
-jovanny.make_withdrawl(70)
-jovanny.display_user_balance()
+jovanny.make_deposit(100).make_withdrawl(10).make_withdrawl(10).make_withdrawl(70).display_user_balance()
 
 # michael.transfer_money(chuy, 100)
+
+
 
 
 
