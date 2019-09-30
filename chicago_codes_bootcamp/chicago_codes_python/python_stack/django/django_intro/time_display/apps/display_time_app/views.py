09a8a30cfbee10f45adcb6a90a166c072ad62044
neo <chuymedina96@gmail.com>
2019-09-30 14:43:44 -0500

    more

--- chicago_codes_bootcamp/chicago_codes_python/python_stack/django/django_intro/time_display/apps/display_time_app/views.py
+++ chicago_codes_bootcamp/chicago_codes_python/python_stack/django/django_intro/time_display/apps/display_time_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from time import gmtime, strftime, localtime
+from time import localtime, strftime
+from datetime import datetime
 
 def index(request):
     context = {
-        "time": strftime("%a, %d %b %Y %I:%M:%S", localtime())
+        "time": strftime("%a, %d %b %Y %I:%M:%S", localtime()),
+        "date": strftime("%b %d, %Y", localtime()),
+        "now" : datetime.now()
     }
     return render(request,'time_display_templates/index.html', context)
-
