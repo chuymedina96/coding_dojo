2ffe7354b27e5a4dc4cba589dc5a8d7d5d96b1d9
neo <chuymedina96@gmail.com>
2019-10-01 11:54:04 -0500

    session required assignments completed. Working on integrading button functionality and form

---
+++ chicago_codes_bootcamp/chicago_codes_python/python_stack/django/django_intro/session_practice/apps/session_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+def index(request):
+    if 'counter' not in request.session:
+        request.session["counter"] = 0
+    else:
+        request.session["counter"] +=1
+
+    context ={
+        'counter': request.session["counter"],
+        'session': request.session
+    }
+
+    return render(request, "session_templates/index.html", context)
+
+def destroy_session(request):
+    request.session.pop("counter")
+    return redirect("/")
+
+
+
