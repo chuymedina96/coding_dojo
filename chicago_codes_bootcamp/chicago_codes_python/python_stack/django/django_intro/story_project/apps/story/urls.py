d188d93b5fe5e7e8edca472eb525480b775d8443
neo <chuymedina96@gmail.com>
2019-10-01 16:19:07 -0500

    more

--- chicago_codes_bootcamp/chicago_codes_python/python_stack/django/django_intro/story_project/apps/story/urls.py
+++ chicago_codes_bootcamp/chicago_codes_python/python_stack/django/django_intro/story_project/apps/story/urls.py
@@ -4,6 +4,13 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^story$', views.story),
-    url(r'^cop$', views.cop1),
-    
+    url(r'^cop1$', views.cop1),
+    url(r'^cop2$', views.cop2),
+    url(r'^cop3$', views.cop3),
+    url(r'^cop4$', views.cop4),
+    url(r'^robber1$', views.robber1),
+    url(r'^robber2$', views.robber2),
+    url(r'^robber3$', views.robber3),
+    url(r'^robber4$', views.robber4)
+
 ]
--- chicago_codes_bootcamp/chicago_codes_python/python_stack/django/django_intro/story_project/apps/story/views.py
+++ chicago_codes_bootcamp/chicago_codes_python/python_stack/django/django_intro/story_project/apps/story/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 def index(request):
@@ -16,8 +16,50 @@ def story(request):
         "request_name": request.session['name'],
         "request_character": request.session['character'],
     }
-    return render(request, "story_templates/begin.html", context)
+    if request.POST['character'] == 'cop':
+        return redirect("/cop1")
+    else:
+        return redirect("/robber1")
 
-def cop1(request):
+def cop1(request): #Cop1 will render cop form for submitting data variables into the story
     return render(request, "story_templates/cop/index.html")
+
+
+def cop2(request):
+    food_from_form              = request.POST['food']
+    vehicle_from_form           = request.POST['vehicle']
+    music_from_form             = request.POST['music']
+    behavior_from_form          = request.POST['behavior']
+    request.session['food']     = food_from_form
+    request.session['vehicle']  = vehicle_from_form
+    request.session['music']    = music_from_form
+    request.session['behavior'] = behavior_from_form
+
+    context ={
+        "food_from_template": food_from_form,
+        "vehicle_from_template": vehicle_from_form,
+        "music_from_template": music_from_form,
+        "behavior_from_form": behavior_from_form,
+        
+    }
+    return render(request, "story_templates/cop/story1.html", context)
+
+def cop3(request):
+    return render(request, "story_templates/cop/story2.html")
+
+def cop4(request):
+    return render(request, "story_templates/cop/story3.html")
+
+
+def robber1(request):
+    return render(request,"story_templates/robber/index.html")
+
+def robber2(request):
+    return render(request, "story_templates/robber/story1.html")
+
+def robber3(request):
+    return render(request, "story_templates/robber/story2.html")
+    
+def robber4(request):
+    return render(request, "story_templates/robber/story3.html")
 
