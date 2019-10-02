8261cf91322885f1d3c96a1918737d129351a2a5
neo <chuymedina96@gmail.com>
2019-10-02 17:33:32 -0500

    more

--- chicago_codes_bootcamp/chicago_codes_python/python_stack/django/django_intro/story_project/apps/story/urls.py
+++ chicago_codes_bootcamp/chicago_codes_python/python_stack/django/django_intro/story_project/apps/story/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^cop2$', views.cop2),
     url(r'^cop3$', views.cop3),
     url(r'^cop4$', views.cop4),
-    url(r'^robber1$', views.robber1),
+    url(r'^robber1$', views.robber2),
     url(r'^robber2$', views.robber2),
     url(r'^robber3$', views.robber3),
     url(r'^robber4$', views.robber4)
--- chicago_codes_bootcamp/chicago_codes_python/python_stack/django/django_intro/story_project/apps/story/views.py
+++ chicago_codes_bootcamp/chicago_codes_python/python_stack/django/django_intro/story_project/apps/story/views.py
@@ -1,48 +1,55 @@
 from django.shortcuts import render, HttpResponse, redirect
 
-# Create your views here.
+# Views start here.
+
 def index(request):
+
     return render(request, "story_templates/index.html")
 
+
 def story(request):
+
     name_from_form                  = request.POST['name']
     radio_from_form                 = request.POST['character']
     request.session['name']         = name_from_form
     request.session['character']    = radio_from_form
 
-    context={
+    context = {
         "name_from_template"        : name_from_form,
         "radio_from_template"       : radio_from_form,
         "request_name"              : request.session['name'],
         "request_character"         : request.session['character'],
+
     }
+
     if request.POST['character'] == 'cop':
         return render(request, "story_templates/cop/index.html", context)
     else:
-        return redirect("/robber1")
+        return render(request, "story_templates/robber/index.html", context)
 
 
 def cop2(request):
-    food_from_form              = request.POST['food']
-    vehicle_from_form           = request.POST['vehicle']
-    music_from_form             = request.POST['music']
-    behavior_from_form          = request.POST['behavior']
-    request.session['food']     = food_from_form
-    request.session['vehicle']  = vehicle_from_form
-    request.session['music']    = music_from_form
-    request.session['behavior'] = behavior_from_form,
+
+    food_from_form                  = request.POST['food']
+    vehicle_from_form               = request.POST['vehicle']
+    music_from_form                 = request.POST['music']
+    request.session['food']         = food_from_form
+    request.session['vehicle']      = vehicle_from_form
+    request.session['music']        = music_from_form
     
 
-    context ={
+    context = {
+
         "food_from_template"        : request.session['food'],
         "vehicle_from_template"     : request.session['vehicle'],
         "music_from_template"       : request.session['music'],
-        "behavior_from_form"        : request.session['behavior'],
         "request_name"              : request.session['name'],
         "request_character"         : request.session['character']
 
     }
+
     return render(request, "story_templates/cop/story1.html", context)
+
 
 def cop3(request):
 
@@ -50,39 +57,79 @@ def cop3(request):
         "food_from_template"        : request.session['food'],
         "vehicle_from_template"     : request.session['vehicle'],
         "music_from_template"       : request.session['music'],
-        "behavior_from_form"        : request.session['behavior'],
         "request_name"              : request.session['name'],
         "request_character"         : request.session['character']
     }
     
     return render(request, "story_templates/cop/story2.html", context)
 
+
 def cop4(request):
 
     context ={
+
         "food_from_template"        : request.session['food'],
         "vehicle_from_template"     : request.session['vehicle'],
         "music_from_template"       : request.session['music'],
-        "behavior_from_form"        : request.session['behavior'],
         "request_name"              : request.session['name'],
         "request_character"         : request.session['character']
     }
+
     return render(request, "story_templates/cop/story3.html", context)
 
 
-def robber1(request):
-    context ={
-        "request_name"              : request.session['name'],
-        "request_character"         : request.session['character']
-    }
-    return render(request,"story_templates/robber/index.html", context)
-
 def robber2(request):
-    return render(request, "story_templates/robber/story1.html")
+
+    time_from_form                  = request.POST['time']
+    rent_from_form                  = request.POST['rent']
+    years_from_form                 = request.POST['years']
+    transport_from_form             = request.POST['transport']
+    request.session['time']         = time_from_form
+    request.session['rent']         = rent_from_form
+    request.session['years']        = years_from_form
+    request.session['transport']    = transport_from_form
+
+    context = {
+
+        "request_name"              : request.session['name'],
+        "request_character"         : request.session['character'],
+        "time"                      : request.session['time'],
+        "rent"                      : request.session['rent'],
+        "years"                     : request.session['years'],
+        "transport"                 : request.session['transport'],
+        
+    }
+    return render(request, "story_templates/robber/story1.html", context)
+
 
 def robber3(request):
-    return render(request, "story_templates/robber/story2.html")
+
+    context = {
+
+        "request_name"              : request.session['name'],
+        "request_character"         : request.session['character'],
+        "time"                      : request.session['time'],
+        "rent"                      : request.session['rent'],
+        "years"                     : request.session['years'],
+        "transport"                 : request.session['transport'],
+        
+    }
+
+    return render(request, "story_templates/robber/story2.html", context)
     
+
 def robber4(request):
-    return render(request, "story_templates/robber/story3.html")
+
+    context = {
+
+        "request_name"              : request.session['name'],
+        "request_character"         : request.session['character'],
+        "time"                      : request.session['time'],
+        "rent"                      : request.session['rent'],
+        "years"                     : request.session['years'],
+        "transport"                 : request.session['transport'],
+        
+    }
+
+    return render(request, "story_templates/robber/story3.html", context)
 
