a57faff8b4a26f4cef2cb3fcbaa8547bee5f0afc
neo <chuymedina96@gmail.com>
2019-10-03 11:59:11 -0500

    more

--- chicago_codes_bootcamp/chicago_codes_python/python_stack/django/django_intro/random_word_generator/apps/random_word/views.py
+++ chicago_codes_bootcamp/chicago_codes_python/python_stack/django/django_intro/random_word_generator/apps/random_word/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello from index")
+    
+    if 'counter' not in request.session:
+        request.session["counter"] = 0
+    else:
+        request.session["counter"] +=1
 
+    unique_id = get_random_string(length=14)
+
+    context ={
+        'counter'       : request.session["counter"],
+        'session'       : request.session,
+        "random_word"   : unique_id
+    }
+    return render(request, "index.html", context)
+
+
+def reset(request):
+    request.session.pop("counter")
+    return redirect("/random_word")
+
+def generate(request):
+    request.session['counter'] +=1
