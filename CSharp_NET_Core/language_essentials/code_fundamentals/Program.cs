7eccbf7243ef84503556e2f2f78ed1a7e7cb472a
neo <chuymedina96@gmail.com>
2019-11-20 14:26:58 -0600

    more c# stuff

--- CSharp_NET_Core/language_essentials/code_fundamentals/Program.cs
+++ CSharp_NET_Core/language_essentials/code_fundamentals/Program.cs
@@ -182,6 +182,66 @@ namespace FirstCSharp
                 Console.WriteLine(bikes[2]); //Prints "BMW"
                 Console.WriteLine($"We currently know of {bikes.Count} motorcycle manufacturers.");
 
+                // Iterating through a list
+                    // Similar to how we were able to access a list just as we had accessed an array, we can also iterate through a list exactly as we had with an array. This all goes back to the fact that, though a list is an object it is nice enough to keep attributes reference-able by indexing through the use of [] brackets.
+
+                    //Using our array of motorcycle manufacturers from before
+                    //we can easily loop through the list of them with a C-style for loop
+                    //this time, however, Count is the attribute for a number of items.
+                    Console.WriteLine("The current manufacturers we have seen are:");
+                    for (var idx = 0; idx < bikes.Count; idx++)
+                    {
+                    Console.WriteLine("-" + bikes[idx]);
+                    }
+                    // Insert a new item between a specific index
+                    bikes.Insert(2, "Yamaha");
+                    //Removal from Generic List
+                    //Remove is a lot like Javascript array pop, but searches for a specified value
+                    //In this case we are removing all manufacturers located in Japan
+                    bikes.Remove("Suzuki");
+                    // bikes.Remove("Yamaha");
+                    bikes.RemoveAt(0); //RemoveAt has no return value however
+                    //The updated list can then be iterated through using a foreach loop
+                    Console.WriteLine("List of Non-Japanese Manufacturers:");
+                    foreach (string manu in bikes)
+                    {
+                    Console.WriteLine("-" + manu);
+                    }
+
+                // Dictionary Class
+                    // A dictionary is the final collection type commonly used in C# that we will discuss. It is implemented very similarly to Lists and also makes use of Generics in its instantiation. The major difference that exists between a list and a dictionary is that dictionary values are not referenced by numerical indexes, but rather key-value pairs. The type of both the key and the value must be specified when initializing a dictionary as such: Dictionary<TKey, TValue>. It also is part of the Generic library, so don't forget to include the using statement from the previous chapter on lists.
+
+                    Dictionary<string,string> profile = new Dictionary<string,string>();
+                    //Almost all the methods that exists with Lists are the same with Dictionaries
+                    profile.Add("Name", "Speros");
+                    profile.Add("Language", "PHP");
+                    profile.Add("Location", "Greece");
+                    Console.WriteLine("Instructor Profile");
+                    Console.WriteLine("Name - " + profile["Name"]);
+                    Console.WriteLine("From - " + profile["Location"]);
+                    Console.WriteLine("Favorite Language - " + profile["Language"]);
+
+                    // One major difference between Lists and Dictionaries when it comes to interacting with them is iterating through their collections. The way we had been using foreach loops with Lists needs to be changed slightly to work with dictionaries because we are working with key-value pairs instead of incremented indices and values. As such, the temp variable that holds the values and how we work with them must be altered slightly:
+
+                    foreach (KeyValuePair<string,string> entry in profile)
+                    {
+                    Console.WriteLine(entry.Key + " - " + entry.Value);
+                    }
+
+                    // This is okay, but that KeyValuePair<string,string> line looks a little scary. We can clean this up by using a pretty cool technique called type inference. Type inference allows us to not have to directly type the type for the variable, but rather will cause the compiler to infer what the type will be based on the very first value assigned to it. A few out there might be feeling "This is awesome! Why weren't we taught this right away!?" This technique can save some time, but it can also greatly reduce clarity that specifying types provides. It is to be used sparingly when the type of a variable can easily be deduced.
+
+                    //The var keyword takes the place of a type in type-inference
+                    foreach (var entry in profile)
+                    {
+                    Console.WriteLine(entry.Key + " - " + entry.Value);
+                    }
+
+
+
+
+
+
+
             
 
                 
