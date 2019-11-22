5c380844055f2d7d89447d8d113dd8b95f4f418b
neo <chuymedina96@gmail.com>
2019-11-22 13:53:13 -0600

    more c sharp basics, easy

---
+++ CSharp_NET_Core/language_essentials/assignments/collections_practice/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace collections_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+
+            // Three Basic Arrays
+                // Create an array to hold integer values 0 through 9
+
+                int[] myArray = {0,1,2,3,4,5,6,7,8,9};
+
+                foreach (int num in myArray)
+                {
+                    Console.WriteLine(num);
+                }
+
+                // Create an array of the names "Tim", "Martin", "Nikki", & "Sara"
+
+                string[] names = new string[] {"Tim", "Martin", "Nikki", "Sara"};
+
+                foreach (string name in names)
+                {
+                    Console.WriteLine(name);
+                }
+
+                // Create an array of length 10 that alternates between true and false values, starting with true
+
+                    // bool myBoolen = true;
+                    // bool[] boolArray = new boolArray{10}; 
+
+            // List of Flavors
+
+                // Create a list of ice cream flavors that holds at least 5 different flavors (feel free to add more than 5!)
+                // Output the length of this list after building it
+                // Output the third flavor in the list, then remove this value
+                // Output the new length of the list (It should just be one fewer!)
+
+                List<string> iceCream = new List<string>();
+
+                iceCream.Add("Chocalate");
+                iceCream.Add("Vanilla");
+                iceCream.Add("Cookies and Cream");
+                iceCream.Add("Pistachio");
+                iceCream.Add("Strawberry");
+                iceCream.Add("Oreo");
+
+                foreach(string flavor in iceCream){
+                    Console.WriteLine(flavor);
+                }
+                Console.WriteLine(iceCream.Count);
+
+                Console.WriteLine(iceCream[2]);
+
+
+
+                    
+
+        }
+    }
+}
