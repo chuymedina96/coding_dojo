638f36ce622759e2e6b5f6be5311927f679b6b52
neo <chuymedina96@gmail.com>
2019-11-23 11:58:38 -0600

    more c sharp

--- CSharp_NET_Core/language_essentials/assignments/collections_practice/Program.cs
+++ CSharp_NET_Core/language_essentials/assignments/collections_practice/Program.cs
@@ -36,26 +36,50 @@ namespace collections_practice
 
             // List of Flavors
 
+
                 // Create a list of ice cream flavors that holds at least 5 different flavors (feel free to add more than 5!)
                 // Output the length of this list after building it
                 // Output the third flavor in the list, then remove this value
                 // Output the new length of the list (It should just be one fewer!)
 
-                List<string> iceCream = new List<string>();
+                    List<string> iceCream = new List<string>();
 
-                iceCream.Add("Chocalate");
-                iceCream.Add("Vanilla");
-                iceCream.Add("Cookies and Cream");
-                iceCream.Add("Pistachio");
-                iceCream.Add("Strawberry");
-                iceCream.Add("Oreo");
+                    iceCream.Add("Chocalate");
+                    iceCream.Add("Vanilla");
+                    iceCream.Add("Cookies and Cream");
+                    iceCream.Add("Pistachio");
+                    iceCream.Add("Strawberry");
+                    iceCream.Add("Oreo");
 
-                foreach(string flavor in iceCream){
-                    Console.WriteLine(flavor);
-                }
-                Console.WriteLine(iceCream.Count);
+                    Console.WriteLine(iceCream.Count);
 
-                Console.WriteLine(iceCream[2]);
+                    Console.WriteLine(iceCream[2]); // This output Cookies and Cream
+                    iceCream.RemoveAt(2);
+
+                    foreach(string flavor in iceCream){
+                        Console.WriteLine(flavor);
+                    }
+                    Console.WriteLine(iceCream.Count);
+
+                    // User Info Dictionary
+
+                    // Create a dictionary that will store both string keys as well as string values
+                    // Add key/value pairs to this dictionary where:
+                    //     each key is a name from your names array
+                    //     each value is a randomly select a flavor from your flavors list.
+                    // Loop through the dictionary and print out each user's name and their associated ice cream flavor
+
+                    Dictionary<string, string> myDict = new Dictionary<string, string>();
+
+                    myDict.Add("Name", "Oreo");
+                    myDict.Add("Name", "Strawberry");
+                    myDict.Add("Name", "Pistachio");
+
+                    foreach (var entry in myDict)
+                    {
+                        Console.WriteLine(entry.Key + " - " + entry.Value);
+                    }
+
 
 
 
